fc88010310492a3279c54927426231749be1d9b5
hyphenperator <pratyushasthana21@gmail.com>
2025-02-03 17:08:32 +0000

    Update server.js

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from AWS EKS Node.js App!");
+  res.send("Hello from AWS EKS Node.js App! - V2");
 });
 
 const PORT = process.env.PORT || 3000;
